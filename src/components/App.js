6468ebbc3cd0f947d1529ea8c13f74b0c2bb09ba
Grey Pegram <dpegram85@gmail.com>
2024-05-28 19:24:22 -0400

    Completed Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+
+    const [dogPic, setDogPic] = useState(null)
+    const API_URL = 'https://dog.ceo/api/breeds/image/random'
+
+
+    useEffect(() => {
+        fetch(API_URL)
+        .then(r => r.json())
+        .then(data => {
+            const image = data.message
+            setDogPic(image)
+        })
+    }, [])
+
+    if(!dogPic) {
+        return <p>Loading...</p>
+    }
+
+    return(
+        <div>
+            <img src={dogPic} alt="A Random Dog" />
+        </div>
+    )
+}
+
+export default App
